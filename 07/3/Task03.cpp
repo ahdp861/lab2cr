8c2f947852bb059f9c575f7ebc2cf922bf824893
ahdp861 <lkssaa862@gmail.com>
2025-04-17 11:20:55 +0300

    Update Task03.cpp

--- 07/3/Task03.cpp
+++ 07/3/Task03.cpp
@@ -1,16 +1,16 @@
-﻿/*Дан односвязный линейный список и указатель на голову списка P1. Необходимо
-вставить значение M после каждого второго элемента списка, и вывести ссылку на последний
-элемент полученного списка P2.
+/*Дан односвязный линейный список и указатель на голову списка P1. Необходимо FIX ME не по шаблону
+ *вставить значение M после каждого второго элемента списка, и вывести ссылку на последний
+ *элемент полученного списка P2.
+ *
+ *Для каждой динамической структуры должен быть предусмотрен стандартный набор методов -
+ *добавления/удаления/вывода элементов.
+ *Во всех задачах обязательно наличие дружественного интерфейса. Ввод данных с клавиатуры.
+ *
+ */
 
-Для каждой динамической структуры должен быть предусмотрен стандартный набор методов - 
-добавления/удаления/вывода элементов. 
-Во всех задачах обязательно наличие дружественного интерфейса. Ввод данных с клавиатуры.
-
-*/
 
 
-
-# include <windows.h>
+//# include <windows.h> FIX ME ненужный include
 # include <iostream>
 using namespace std;
 
@@ -22,113 +22,119 @@ public:
 };
 typedef Node* PNode;
 
-void add(int data, PNode& Head, PNode& LastNode)
+//void add(int data, PNode& Head, PNode& LastNode) FIX ME некорректное название
+void add_node(int data, PNode& head, PNode& last_node)
 {
-	PNode Temp;
-	if (Head == nullptr)
+	PNode temporary_node;
+	if (head == nullptr)
 	{
-		Head = new Node;
-		LastNode = Head;
-		Head->next = nullptr;
+		head = new Node;
+		last_node = head;
+		head->next = nullptr;
 	}
 	else
 	{
-		Temp = new Node;
-		LastNode->next = Temp;
-		LastNode = Temp;
-		LastNode->next = nullptr;
+		temporary_node = new Node;
+		last_node->next = temporary_node;
+		last_node = temporary_node;
+		last_node->next = nullptr;
 	}
-	LastNode->x = data;
+	last_node->x = data;
 }
 
-void show(PNode& Head)
+//void Show(PNode& Head) FIX ME некорректное название
+void show(PNode& head)
 {
-	PNode Mynode = Head;
+	//PNode Mynode = head; FIX ME некорректное название
 	cout << "Все числа из списка: ";
 
-	while (Mynode != nullptr)
+	while (my_node != nullptr)
 	{
-		cout << Mynode->x << " ";
-		Mynode = Mynode->next;
+		cout << my_node->x << " ";
+		my_node = my_node->next;
 	}
 
 }
 
-void addafter(int data, PNode& Head)
+//void addafter(int data, PNode& head) FIX ME некорректное название
+void add_after(int data, PNode& head)
 {
-	PNode Temp = Head;
-	PNode MyNode;
+	//PNode Temp = head; FIX ME некорректное название
+	PNode temporary_node = head;
+	PNode my_node;
 	int count = 1;
-	while (Temp != nullptr)
+	while (temporary_node != nullptr)
 	{
-		count++;
+		//count++; FIX ME префиксная запись
+		++count;
 		if (count == 4)
 		{
-			MyNode = new Node;
-			MyNode->x = data;
-			MyNode->next = Temp->next;
-			Temp->next = MyNode;
+			my_node = new Node;
+			my_node->x = data;
+			my_node->next = temporary_node->next;
+			temporary_node->next = my_node;
 			count = 0;
 		}
-		Temp = Temp->next;
+		temporary_node = temporary_node->next;
 	}
 }
 
-void showafter(PNode& Head)
+//void showafter(PNode& head) FIX ME некорректное название
+void show_after(PNode& head)
 {
-	PNode Mynode = Head;
+	PNode my_node = head;
 	cout << endl;
 	cout << "Все числа из измененного списка: ";
-	while (Mynode != nullptr)
+	while (my_node != nullptr)
 	{
-		cout << Mynode->x << " ";
-		Mynode = Mynode->next;
+		cout << my_node->x << " ";
+		my_node = my_node->next;
 		
 	}
 	cout << endl;
 }
 
-PNode showlast(PNode& Head)
+//PNode showlast(PNode& head) FIX ME некорректное название
+PNode show_last(PNode& head)
 {
-	PNode Mynode2 = Head;
-	while (Mynode2 != nullptr && Mynode2->next != nullptr)
+	//PNode my_node_2 = head; FIX ME некорректное название
+	PNode my_node_2 = head;
+	while (my_node_2 != nullptr && my_node_2->next != nullptr)
 	{
-		Mynode2 = Mynode2->next;
+		my_node_2 = my_node_2->next;
 	}
-	return Mynode2;
+	return my_node_2;
 }
 
-PNode Find(PNode Head, int data)
+//PNode find(PNode head, int data) FIX ME некорректное название
+PNode find_node(PNode head, int data)
 {
-	PNode q = Head;
-	while (q && q->x != data)
-		q = q->next;
-	return q;
+	//PNode node = head; FIX ME некорректное название
+	PNode node = head;
+	while (node && node->x != data)
+		node = node->next;
+	return node;
 }
 
 
-void DeleteNode(PNode& Head, PNode OldNode)
+//void DeleteNode(PNode& head, PNode OldNode) FIX ME некорректное название
+void delete_node(PNode& head, PNode old_node)
 {
-	PNode q = Head;
-	if (Head == OldNode)
-		Head = OldNode->next;  // удаляем первый элемент 
+	PNode node = head;
+	if (head == old_node)
+		head = old_node->next;  // удаляем первый элемент 
 	else
 	{
-		while (q->next != OldNode) // ищем элемент 
-			q = q->next;
-		q->next = OldNode->next;
+		while (node->next != old_node) // ищем элемент 
+			node = node->next;
+		node->next = old_node->next;
 	}
-	delete OldNode; // освобождаем память 
+	delete old_node; // освобождаем память 
 }
 
-
-
-int main()
-{
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	PNode H1 = NULL;
-	PNode LastNode = NULL;
+PNode input() {
+	PNode list = NULL;
+	PNode last_node = NULL;
 	cout << "Добро пожаловать!" << endl;
 	cout << "Перед нами задача: " << endl;
 	cout << "Дан односвязный линейный список" << endl;
@@ -147,18 +153,50 @@ int main()
 	for (int i = 1; i <= n; i++)
 	{
 		cin >> x;
-		add(x, H1, LastNode);
+		add(x, list, last_node);
 	}
 	cout << endl;
+	return list;
+}
+
+int main()
+{
+	//SetConsoleCP(1251);	FIX ME не разрешено
+	//SetConsoleOutputCP(1251);
+	/*PNode list = NULL;	FIX ME перенести из main, некорректные названия
+	PNode last_node = NULL;	
+	cout << "Добро пожаловать!" << endl;
+	cout << "Перед нами задача: " << endl;
+	cout << "Дан односвязный линейный список" << endl;
+	cout << "Необходимо вставить значение M после каждого третьего элемента списка," << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "Вам необходимо ввести два значения: " << endl;
+	cout << "1. n(n>=3) - количество элементов в списке" << endl;
+	cout << "2. m - число, которое мы будем вставлять. " << endl;
+	cout << "Затем через пробел ввести элементы списка, количество которых будет совпадать с тем, что вы ввели ранее," << endl;
+	cout << "После мы выведем полученный результат на экран и ссылку на последний элемент. " << endl;
+	cout << "--------------------------------------------------------" << endl;
+	cout << "Введите число(БОЛЬШЕ 3) - количество элементов в массиве: ";
+	int n, x, m;;
+	cin >> n;
+	cout << "Введите " << n << " элементов списка ";
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> x;
+		add(x, list, last_node);
+	}
+	cout << endl;
+	*/
+	PNode list = input();
 	cout << "Введите значение m для вставки после каждого третьего элемента: ";
 	cin >> m;
 	cout << endl;
-	show(H1);
+	show(list);
 	cout << endl;
-	addafter(m, H1);
-	showafter(H1);
+	add_after(m, list);
+	show_after(list);
 	cout << endl;
-	PNode last = showlast(H1);
+	PNode last = show_last(list);
 	cout << "Ссылка на последний элемент: " << last << " " << last->x;
 
 	int choice;
@@ -175,17 +213,18 @@ int main()
 		cout << "Введите число - количество элементов которое вы хотите удалить: ";
 		int num;
 		cin >> num;
-		for (int i = 0; i < num; i++)
+		//for (int i = 0; i < num; i++) FIX ME постфиксная запись
+		for (int i = 0; i < num; ++i)
 		{
 			cout << "Введите значение элемента, который вы хотите удалить: ";
 			int x;
 			cin >> x;
-			PNode p = Find(H1, x);
+			PNode p = find_node(list, x);
 			cout << endl;
 			cout << "Удаление...." << endl;
-			DeleteNode(H1, p);
+			delete_node(list, p);
 		}
-		show(H1);
+		show(list);
 	}break;
 
 	case 2:
@@ -197,7 +236,8 @@ int main()
 	}
 	default:
 	{
-		cout << "Введено неверное число"; break;
+		cout << "Введено неверное число";
+		break;
 	}
 	}
 	return 0;
