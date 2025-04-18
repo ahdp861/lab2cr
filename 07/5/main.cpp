04e56e21a99e2262de18ebfeae74b4d2bcb9fa83
ahdp861 <lkssaa862@gmail.com>
2025-04-18 14:29:38 +0500

    Update main.cpp

--- 07/5/main.cpp
+++ 07/5/main.cpp
@@ -7,20 +7,20 @@ int main() {
     list <int> my_list;
     cout << "Введите размерность дека n1 (n1 >= 5): ";
     cin >> n1;
-    input(my_deque, n1);
+    Input(my_deque, n1);
     cout << "Введите размерность списка n2 (n2 >= 5): ";
     cin >> n2;
-    input(my_list, n2);
+    Input(my_list, n2);
     cout << "Изначальное содержимое контейнеров." << endl;
     cout << "Дек: ";
-    print_elements(my_deque);
+    PrintElements(my_deque);
     cout << "Список: ";
-    print_elements(my_list);
-    insert(my_deque, my_list);
+    PrintElements(my_list);
+    InsertElements(my_deque, my_list);
     cout << "Изменённое содержание контейнеров." << endl;
     cout << "Дек: ";
-    print_elements(my_deque);
+    PrintElements(my_deque);
     cout << "Список: ";
-    print_elements(my_list);
+    PrintElements(my_list);
     return 0;
 }
