c1312c422047dc08287e3b6fbbb842c2b3761983
ahdp861 <lkssaa862@gmail.com>
2025-04-18 14:28:55 +0500

    Update containers.h

--- 07/5/containers.h
+++ 07/5/containers.h
@@ -10,13 +10,13 @@ using namespace std;
 
 template <typename container>
 //void Input(container& N, int n)//Заполнение контейнеров FIX ME некорректное название
-void input(container& container, int container_size);
+void Input(container& container, int container_size);
 
 template <typename container>
 //void Print(const container& container)//Вывод содержимого контейнеров FIX ME некорректное название
-void print_elements(const container& container);
+void PrintElements(const container& container);
 
 //void Insert(deque<int>& D, list<int>& L)//Вставка перед 5 с конца элемента списка последних 5 элементов дека в обратном порядке FIX ME некоректное название
-void insert(deque<int>& my_deque, list<int>& my_list);
+void InsertElement(deque<int>& my_deque, list<int>& my_list);
 
 #endif
